23682508f9a6f525e2737473bfded2683546f1f3
Margaret Retzinger <maretzinger@gmail.com>
2021-12-09 16:10:47 -0500

    Added Part 1 answer

---
+++ Days/1/One.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Days
+{
+    public class One
+    {
+        public void Run()
+        {
+            ParsePartTwo();
+        }
+
+
+        public void ParsePartOne()
+        {
+            StreamReader file = new StreamReader("\\Data.txt");
+            string line;
+            List<int> depthMeasurements = new List<int>();
+            int index = 0;
+            int finalCount = 0;//1521
+
+            while ((line = file.ReadLine()) != null)
+            {
+                depthMeasurements.Add(int.Parse(line));
+            }
+
+            foreach (int item in depthMeasurements)
+            {
+                index += 1;
+                if (index < depthMeasurements.Count() && item < depthMeasurements[index])
+                {
+                    finalCount++;
+                }
+
+            }
+
+            Console.WriteLine(finalCount);
+        }
+
+        public void ParsePartTwo()
+        {
+            StreamReader file = new StreamReader("\\Data.txt");
+            string line;
+            List<int> depthMeasurements = new List<int>();
+            int index = 0;
+            int finalCount = 0;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                depthMeasurements.Add(int.Parse(line));
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Days;
+using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new One().Run();
+            Console.WriteLine("Finished!");
+        }
+    }
+}
